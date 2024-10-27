a525eedcf85605e9b75bcc3f0b7df9e7c6a03a31
Cody King <122841869+Cjking57893@users.noreply.github.com>
2024-10-27 18:52:46 -0400

    Added function for updating items

--- lib/utility.php
+++ lib/utility.php
@@ -38,3 +38,31 @@ function addItem($filename, $newItem) {
     // Writing the updated JSON back in the file.
     file_put_contents($filename, $jsonData);
 }
+
+function updateItem($filename, $oldItem, $newItem) {
+
+    // Checking if file exists and reading it if it does.
+    if (file_exists($filename)) {
+        $jsonContent = file_get_contents($filename);
+        $data = json_decode($jsonContent, true);
+    }
+    else {
+        die('Error: Could not read the JSON file');
+    }
+
+    $itemFound = false;
+    foreach($data as $key => $item) {
+        if ($item === $oldItem) {
+            $data[$key] = $newItem;
+            $itemFound = true;
+            break;
+        }
+    }
+
+    if ($itemFound) {
+        die('Error: Item not found');
+    }
+
+    $jsonData = json_encode($data, JSON_PRETTY_PRINT);
+    file_put_contents($filename, $jsonData);
+}
