0640fb746f665fed70617c2fb8d12ce75711c798
Cody King <122841869+Cjking57893@users.noreply.github.com>
2024-10-27 18:53:46 -0400

    Removed commented out code

--- lib/utility.php
+++ lib/utility.php
@@ -1,22 +1,4 @@
 <?php
-// <!-- create($entity):
-
-// Adds a new entity (a contact or team member) to the JSON file.
-// It reads the existing data, adds the new entity to the array, and writes the updated data back to the file.
-
-// readAll():
-
-// Reads all entities from the JSON file and returns them as an array.
-// If the file doesn’t exist, it returns an empty array.
-
-// update($updatedEntity, $id):
-
-// Finds an entity by its ID in the JSON data, updates it with the new information, and saves the changes back to the file.
-
-// delete($id):
-
-// Removes an entity from the JSON file based on its ID.
-// It filters the array to exclude the specified entity and then writes the filtered data back to the file. -->
 
 function addItem($filename, $newItem) {
 
